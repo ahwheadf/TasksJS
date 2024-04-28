bbea5b45f34529f5f428274f9e9685e179f77f84
ahwheadf <114777210+ahwheadf@users.noreply.github.com>
2024-04-29 00:51:16 +0300

    Точка с запятой

--- 4-navigator/4.11.js
+++ 4-navigator/4.11.js
@@ -3,9 +3,9 @@
 представляя расчет как нахождение диагонали прямоугольника (используя основы тригонометрии или геометрии).
 */
 
-const positionLat = 2
-const positionLong = 1
-const addressLat = 7
-const addressLong = 5
+const positionLat = 2;
+const positionLong = 1;
+const addressLat = 7;
+const addressLong = 5;
 
 console.log(((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2) ** 0.5);
